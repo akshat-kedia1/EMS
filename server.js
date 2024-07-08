76639819b90f93b801ac74e5938f490cefc5903b
AKSHAT KEDIA <akshat_kedia@kedias-MacBook-Pro.local>
2024-07-08 11:38:16 +0530

    1

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
+const dotenv = require('dotenv')
+const colors = require('colors')
+//config dotenv file
+dotenv.config();
+
+//rest objects
+const app =express()
+
+//middleware
+
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(cors())
+
+//routes
+app.get('/',(req,res)=>{
+    res.send("<h1>Hello World</h1>")
+})
+
+//port
+const PORT=8080 || process.env.PORT
+
+//Listen
+app.listen(PORT,()=>{
+    console.log(`Server running on port ${PORT}`)
+})
