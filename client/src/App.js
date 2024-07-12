1c64fa772a371958b95fa77f4ee728253c886f3c
AKSHAT KEDIA <akshat_kedia@kedias-MacBook-Pro.local>
2024-07-12 11:36:46 +0530

    5

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import { Route, Routes } from "react-router-dom"; 
+
+function App() {
+  return (
+    <>
+    <h1>Hello</h1>
+      {/* <Routes>
+        <Route path='/' element={<HomePage />} />
+      </Routes> */}
+    </>
+  );
+}
+
+export default App;
